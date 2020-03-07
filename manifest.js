bf3e5f7c8dfdf10cc5a871a26155cccdb5364019
David Elaine Alt <elaine@flaminghakama.com>
2020-03-07 15:02:03 -0800

    moving manifest to pdf

---
+++ manifest.js
@@ -0,0 +1,98 @@
+/*
+ *  Song specificiation for Alt Jazz song Next Level Shit 
+ */
+
+var altJazzSongs = altJazzSongs || {};
+
+altJazzSongs["Next Level Shit"] = {
+
+    partsInBooks: {
+        "Flute, Alto Flute & Piccolo": {
+            "Next Level Shit": [ "Flute" ], 
+        },
+        "English Horn": {
+            "Next Level Shit": [ "English Horn in F" ], 
+        },
+        "Bass Clarinet": {
+            "Next Level Shit": [ "Bass Clarinet in Bb" ],
+        },
+        "Voice/Percussion": {
+            "Next Level Shit": [ "Voice" ],
+        },
+        "Drums": {
+            "Next Level Shit": [ "Drums" ], 
+        },
+        "Violin I": {
+            "Next Level Shit": [ "Violin I" ], 
+        },
+        "Violin II": {
+            "Next Level Shit": [ "Violin II" ], 
+        },
+        "Viola": {
+            "Next Level Shit": [ "Viola" ], 
+        },
+        "'Cello": {
+            "Next Level Shit": [ "'Cello" ],
+        },
+        "Bass": {
+            "Next Level Shit": [ "Bass" ], 
+        }
+    },
+
+    songs: {
+
+        "Next Level Shit": {
+            metadata: { 
+                title: "Next Level Shit",
+                composer: "Elaine Paul",
+                arranger: "Elaine Paul",
+                genre: "Afro-Cuban Jazz",
+                bpm: '216',
+                key: "Ab"
+            },
+            fileLocation: "next-level-shit",
+            filePrefix: "next-level-shit-",
+            parts: {
+                "Flute": { fileSuffix: "Flute" },
+                "English Horn in F": { fileSuffix: "English-Horn-in-F" },
+                "Bass Clarinet in Bb": { fileSuffix: "Bass-Clarinet-in-Bb" },
+                "Trumpet in Bb": { fileSuffix: "Trumpet-in-Bb" },
+                "Horn in F": { fileSuffix: "Horn-in-F" },
+                "Trombone": { fileSuffix: "Trombone" },
+                "Clave": { fileSuffix: "Clave" },
+                "Piano": { fileSuffix: "Piano" },
+                "Voice": { fileSuffix: "Voice" },
+                "Violin I": { fileSuffix: "Violin-I" },
+                "Violin II": { fileSuffix: "Violin-II" },
+                "Viola": { fileSuffix: "Viola" },
+                "Cello": { fileSuffix: "Cello" },
+                "Bass": { fileSuffix: "Bass" },
+            },
+            scores: {
+                "Rhythm": { fileSuffix: "Rhythm" },
+                "Condensed Score": { fileSuffix: "Condensed-Score" },
+                "Score Transposed": { fileSuffix: "Score-Transposed" },
+                "Score Concert": { fileSuffix: "Score-Concert" },
+                "Lead Sheet": { fileSuffix: "Lead-Sheet" },
+            },
+            recordings: {
+                demo: {
+                    description: 'demo',
+                    url: ''
+                },
+                reading: {
+                    description: 'reading',
+                    url: ''
+                },
+                recording: {
+                    description: 'recording',
+                    url: ''
+                },
+                live: {
+                    description: 'live',
+                    url: ''
+                }
+            }
+        }
+    }
+};
